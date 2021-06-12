cf9b628e9b2b6693c37e61fede0e996f0d3bc478
romer8 <gio.busrom@gmail.com>
2021-06-12 14:16:07 -0600

    added prompt for the different projections

--- tethysapp/metdataexplorer2/public/js/main.js
+++ tethysapp/metdataexplorer2/public/js/main.js
@@ -1,8 +1,16 @@
 var MET_DATA_EXPLORER_PACKAGE = (function(){
 
-
-
   $(function(){
+    $("#btn-info-epsg").on("click", function(){
+      if($("#info-epsg").attr("class") == "hidden"){
+        $("#info-epsg").removeClass("hidden");
+      }
+      else{
+        $("#info-epsg").addClass("hidden");
+      }
+
+    })
+
     load_groups_start();
   })
 
